6f8f26db694d3cf856d5c6f8c807b808043a2862
Devon Casey <devon_casey@outlook.com>
2026-05-09 23:20:25 -0700

    Automated Build Update: 2026-05-09 23:20:18

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Handles local caching of core assets to enable offline playability and rapid subsequent load times.
  */
 
-const CACHE_NAME = 'orbital-trading-1777933653906';
+const CACHE_NAME = 'orbital-trading-1778393552509';
 
 const ASSETS_TO_CACHE = [
     '/',
